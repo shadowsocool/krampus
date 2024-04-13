0eadd885260f6263c6faafbc238a47608d80ee37
shadowwrblx <104780811+shadowwrblx@users.noreply.github.com>
2024-04-13 14:19:28 +0200

    Create connection.lua

---
+++ connection.lua
@@ -0,0 +1,19 @@
+repeat task.wait() until game:IsLoaded()
+
+while task.wait() do 
+    pcall(function()
+        local connection = websocket.connect("http://localhost:6547/")
+
+        connection:Send("CONNECT")
+
+        connection.OnMessage:Connect(function(message)
+            if message == "2"  then
+                connection:Send("3")
+            else   
+                loadstring(message)
+            end
+        end)
+
+        connection.OnClose:Wait()
+    end)
+end
